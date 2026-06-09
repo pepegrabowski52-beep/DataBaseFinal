328922f5a88221ec8448d0ba8f8e886eb8985070
pepegrabowski52-beep <pepegrabowski52@gmail.com>
2026-06-09 13:19:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import os
-import psycopg2
-from psycopg2.extras import DictCursor
+import DictCursor
 import time
 import uuid
 from flask import Flask, render_template, request, session, redirect, url_for
